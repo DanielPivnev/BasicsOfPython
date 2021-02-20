5a025450f7ff46e544d4d36b953822f9b57b43ca
DanielPivnev <d.pivnev2003@gmail.com>
2021-02-20 16:29:56 +0100

    task 2 finished

--- lesson2_dz3.py
+++
@@ -1,9 +0,0 @@
-sentenceList = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-
-for item in sentenceList:
-    if item == sentenceList[1] or item == sentenceList[3]:
-        sentenceList[sentenceList.index(item)] = f'"{int(item):02d}"'
-    elif item == sentenceList[-2]:
-        sentenceList[sentenceList.index(item)] = f'"+{int(item):02d}"'
-
-print(' '.join(sentenceList))
