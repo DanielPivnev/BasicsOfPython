2dc4ffe60c8264a2b6c184218cdae92e644f2e43
DanielPivnev <d.pivnev2003@gmail.com>
2021-03-17 19:43:23 +0100

    task  finished

--- lesson8_dz1.py
+++ lesson8_dz1.py
@@ -3,7 +3,7 @@ import re
 
 def email_parses(email):
      try:
-          email_parse = re.compile(r'((?:\w\.?-?)+)@((?:\w-?)+\.\w+)')
+          email_parse = re.compile(r'((?:\w?\.?-?)+)@((?:\w?-?)+\.\w+)')
           email_parsed = email_parse.findall(email)[0]
           dicti = {
                'username': email_parsed[0],
