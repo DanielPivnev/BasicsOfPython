3be0a523b616d0b9d916284870053c37a1beae39
DanielPivnev <d.pivnev2003@gmail.com>
2021-03-25 09:27:59 +0100

    task  finished

--- lesson10_dz2.py
+++ lesson10_dz2.py
@@ -1,13 +1,28 @@
 import abc
 
 
-class Cloths:
+class Clothes:
     def __init__(self, n):
         self.clothes_name = n
 
     @abc.abstractmethod
-    def material_amount(self):
+    def material_amount(self, x):
         pass
 
 
-class Caot(Cloths):
+class Coat(Clothes):
+    def material_amount(self, v):
+        return v/6.5+0.5
+
+
+class Costume(Clothes):
+    def material_amount(self, h):
+        return 2*h+0.3
+
+
+a = Costume('Mike')
+b = a.material_amount(16)
+print(b)
+c = Coat('abc')
+d = c.material_amount(28)
+print(d)
