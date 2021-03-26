f240f702d5b8bd6fb8915fbf0b7d0de8b1cd711c
DanielPivnev <d.pivnev2003@gmail.com>
2021-03-26 17:10:04 +0100

    task 3 finished

--- lesson11_dz3.py
+++ lesson11_dz3.py
@@ -9,17 +9,18 @@ def str_to_float(str_num):
     except:
         raise NotANumber('Enter please the next time a just number.')
 
-inputed_txt = ''
+
+inputted_txt = ''
 number_list = []
 a = True
 while a:
     try:
-        inputed_txt = input('Enter please just a number which you want add to the list '
+        inputted_txt = input('Enter please just a number which you want add to the list '
                                   'or if you are ready enter "stop": ')
-        if inputed_txt == 'stop':
+        if inputted_txt == 'stop':
             a = False
         else:
-            number_list.append(str_to_float(inputed_txt))
+            number_list.append(str_to_float(inputted_txt))
     except NotANumber as e:
         print(e)
 print(f'Here are your list of numbers: {number_list} \nThank you! Jesus love you!')
